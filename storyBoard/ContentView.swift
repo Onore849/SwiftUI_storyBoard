2ecd2ca3223129a00986b9bb34f02907188e681d
Onore849 <rion3376@gmail.com>
2020-09-04 12:39:36 +0900

    Initial Commit

---
+++ storyBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  storyBoard
+//
+//  Created by 野澤拓己 on 2020/09/04.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
